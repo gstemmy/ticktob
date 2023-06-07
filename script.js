4dc20f47cefd4eeddb116555d9c9085aae8fe5f2
gstemmy <gbolahansaheedt@gmail.com>
2023-06-07 09:47:37 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+
+let formbtn = document.querySelector("#transfer");
+let backbtn = document.querySelector("#back");
+let middlecontainer = document.querySelector(".ticket");
+let middlecontainer2 = document.querySelector(".ticket01");
+let middlecontainer1 = document.querySelector(".ticket02");
+
+
+// formbtn.onclick = () =>{
+//     middlecontainer1.classList.add('active');
+//     middlecontainer2.classList.add('active');
+    
+//     setTimeout(() => {
+//        middlecontainer.classList.add('active');
+
+//    }, 2000)
+
+// }
+
+
+window.addEventListener("load", function (){
+    formbtn.addEventListener("click", function() {
+        middlecontainer.style.display = "block";
+        setTimeout(function() {
+            middlecontainer.style.display = "none";
+            middlecontainer1.style.display = "none";
+            middlecontainer2.style.display = "block";
+        }, 2000);
+    });
+});
+
+
+
+
+
+backbtn.onclick = () =>{
+   middlecontainer2.style.display = "none";
+   middlecontainer.style.display = "none";
+   middlecontainer1.style.display = "block";
+
+}
